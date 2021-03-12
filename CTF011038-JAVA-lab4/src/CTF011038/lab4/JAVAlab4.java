1ebd20d0fbf63bcfb8acc3f7c0c3972f1265c4e6
TheNUI <51776857+TheNUI@users.noreply.github.com>
2021-03-12 15:58:24 +0700

    JAVA lab4

---
+++ CTF011038-JAVA-lab4/src/CTF011038/lab4/JAVAlab4.java
@@ -0,0 +1,26 @@
+package CTF011038.lab4;
+
+public class JAVAlab4 {
+	public static void main(String [] args) {
+//1
+		int score = 60;
+		if(score>=80) {
+		System.out.println("Good");		
+		}else if(score>=50&&score<80) {
+			System.out.println("normal");
+			}else {
+				System.out.println("fail");
+			}
+//2
+		boolean isHandSome = false;
+		if(isHandSome) {
+			System.out.println("Good");
+				}else if(score>=80) {
+				System.out.println("Good");		
+					}else if(score>=50&&score<80) {
+						System.out.println("normal");
+						}else {
+					System.out.println("fail");
+					}
+	}
+}
